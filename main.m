1c4d7a9134e5c7e6cb8eba2d5fa138f23a2d08b1
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2023-12-11 16:05:40 +0000

    Data pre-processing

--- main.m
+++ main.m
@@ -10,23 +10,24 @@ labels = digitTable(:, 65);
 digitTable(:, 65) = [];
 
 totalNums = height(digitTable);
-
-reshapedDigitTable = cell(size(digitTable, 1) , 1);
-
-for i = 1 : size (digitTable, 1)
-    reshapedDigitTable{i} = reshape(data(i, :), [8, 8])
+allDigits = zeros(8,8,totalNums); % 3D matrix for all digits
+figure;
+for i = 1 : 20 % size (digitTable, 1)
+    currCol = reshape(table2array(digitTable(i, :)), 8, 8);
+    currCol = currCol';
+    allDigits(:, :, i) = currCol;
+    if i < 10
+        subplot(3, 3, i);
+    end
+    
+    imagesc(currCol);
+    colormap(gray);
+    colorbar;
 
 end
 
-numericDigitTable = table2array(reshapedDigitTable);
 
-for i = 1 : size(reshapedDigitTable, 1)
-   
 
-end
-
-imagesc(numericDigitTable);
-colormap(gray);
 
 % b. Divide the dataset into training and test datasets, cross-validation
 % and k-fold
