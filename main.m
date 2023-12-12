91a9facc6380e24caa8ad70ec94441bc275573f5
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2023-12-12 16:30:54 +0000

    Non-functioning Neural Network

--- main.m
+++ main.m
@@ -1,12 +1,11 @@
 % Tooboxes: Image Processing Toolbox
-
 clc;
 close all;
 clear;
 
 % a. Loading data and preprocessing
 digitTable = readtable('handwritten_digits.csv', 'Headerlines', 1);
-labels = digitTable(:, 65);
+labels = table2array(digitTable(:, 65));
 digitTable(:, 65) = [];
 
 totalNums = height(digitTable);
@@ -31,5 +30,56 @@ end
 
 % b. Divide the dataset into training and test datasets, cross-validation
 % and k-fold
-train = 0.75; % 75% training data
+trainRatio = 0.75; % 75% training data
+
+folds = 5;
+
+c = cvpartition(labels, "KFold", folds);
+
+% c. Creating NN
+layers = [
+    imageInputLayer([8 8 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+for i = 1:folds
+    train = training(c, i);
+    validation = test(c, i);
+
+    trainData = allDigits(:, : , train);
+    trainLabels = categorical(labels(train)');
+
+    validationData = allDigits(:, :, validation);
+    validationLabels = categorical(labels(validation)');
+    
+    options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs',4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData', {validationData, validationLabels}, ...
+    'ValidationFrequency',30, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+    net = trainNetwork(trainData, trainLabels, layers, options);
+    predictions = classify(net, validationData);
+end
 
